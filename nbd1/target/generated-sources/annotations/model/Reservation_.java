f8b5677e311a68984e5b4bb0da9feeb7e82b26d1
236657 Wojciech Stefaniak <236657@edu.p.lodz.pl>
2022-10-20 00:05:39 +0200

    maven plugin fixed

--- nbd1/target/generated-sources/annotations/model/Reservation_.java
+++ nbd1/target/generated-sources/annotations/model/Reservation_.java
@@ -10,17 +10,17 @@ import javax.annotation.processing.Generated;
 @StaticMetamodel(Reservation.class)
 public abstract class Reservation_ extends model.AbstractEntity_ {
 
-	public static volatile SingularAttribute<Reservation, Long> reservation_id;
 	public static volatile SingularAttribute<Reservation, LocalDate> date;
 	public static volatile SingularAttribute<Reservation, LocalTime> endHour;
 	public static volatile SingularAttribute<Reservation, Parent> parent;
+	public static volatile SingularAttribute<Reservation, Long> reservationId;
 	public static volatile SingularAttribute<Reservation, LocalTime> startHour;
 	public static volatile SingularAttribute<Reservation, Sitter> sitter;
 
-	public static final String RESERVATION_ID = "reservation_id";
 	public static final String DATE = "date";
 	public static final String END_HOUR = "endHour";
 	public static final String PARENT = "parent";
+	public static final String RESERVATION_ID = "reservationId";
 	public static final String START_HOUR = "startHour";
 	public static final String SITTER = "sitter";
 
