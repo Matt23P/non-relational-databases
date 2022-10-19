addcbe1d39603bc983e629490db9c54d9d98b159
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-10-19 19:24:24 +0200

    implementation of SitterRepository.java

--- nbd1/src/main/java/repositories/SitterRepository.java
+++ nbd1/src/main/java/repositories/SitterRepository.java
@@ -1,4 +1,30 @@
 package repositories;
 
-public class SitterRepository {
+import entity.EntityManagerCreator;
+import jakarta.persistence.EntityManager;
+import model.Sitter;
+
+public class SitterRepository implements Repository<Sitter, Long> {
+    public Sitter add(Sitter object) {
+        try (EntityManager manager = EntityManagerCreator.getEntityManager()) {
+            manager.getTransaction().begin();
+            manager.persist(object);
+            manager.getTransaction().commit();
+            return object;
+        }
+    }
+
+    public Sitter get(Long id) {
+        try (EntityManager manager = EntityManagerCreator.getEntityManager()) {
+            return manager.find(Sitter.class, id);
+        }
+    }
+
+    public void remove(Sitter object) {
+        try (EntityManager manager = EntityManagerCreator.getEntityManager()) {
+            manager.getTransaction().begin();
+            manager.remove(manager.merge(object));
+            manager.getTransaction().commit();
+        }
+    }
 }
