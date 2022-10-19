7a0feae1e2a9a1a5a01f7f1cf6ad0935db29d79f
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-10-19 19:51:50 +0200

    ReservationRepository.java implemented

--- nbd1/src/main/java/repositories/ReservationRepository.java
+++ nbd1/src/main/java/repositories/ReservationRepository.java
@@ -1,4 +1,33 @@
 package repositories;
 
-public class ReservationRepository {
+import entity.EntityManagerCreator;
+import jakarta.persistence.EntityManager;
+import model.Reservation;
+
+public class ReservationRepository implements Repository<Reservation, Long>{
+    @Override
+    public Reservation add(Reservation object) {
+        try(EntityManager manager = EntityManagerCreator.getEntityManager()) {
+            manager.getTransaction().begin();
+            manager.persist(object);
+            manager.getTransaction().commit();
+            return object;
+        }
+    }
+
+    @Override
+    public Reservation get(Long id) {
+        try(EntityManager manager = EntityManagerCreator.getEntityManager()) {
+            return manager.find(Reservation.class, id);
+        }
+    }
+
+    @Override
+    public void remove(Reservation object) {
+        try(EntityManager manager = EntityManagerCreator.getEntityManager()) {
+            manager.getTransaction().begin();
+            manager.remove(manager.merge(object));
+            manager.getTransaction().commit();
+        }
+    }
 }
