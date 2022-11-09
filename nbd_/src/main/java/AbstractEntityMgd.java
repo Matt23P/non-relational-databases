0e062854a83b1eef96b16cfff6be65b04c0077d1
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-11-09 13:32:12 +0100

    classes that will help us in mapping - created and implemented; need to find out how UniqueIdMgd class should work like

---
+++ nbd_/src/main/java/AbstractEntityMgd.java
@@ -0,0 +1,16 @@
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
+import java.io.Serializable;
+
+public abstract class AbstractEntityMgd implements Serializable {
+    @BsonProperty("_id")
+    private final UniqueIdMgd entityId;
+
+    public UniqueIdMgd getEntityId() {
+        return entityId;
+    }
+
+    public AbstractEntityMgd(UniqueIdMgd entityId) {
+        this.entityId = entityId;
+    }
+}
