07480beff84ec08551b514e0db3c8b92d7076329
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-11-09 12:21:51 +0100

    AbstractMongoRepository fixed; added some imports and the code isnt red anymore

--- nbd_/src/main/java/AbstractMongoRepository.java
+++ nbd_/src/main/java/AbstractMongoRepository.java
@@ -1,13 +1,21 @@
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoCredential;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
 import com.sun.tools.javac.util.List;
+import org.bson.codecs.configuration.CodecRegistries;
+import org.bson.codecs.pojo.Conventions;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import org.bson.codecs.configuration.CodecRegistry;
 
 public abstract class AbstractMongoRepository implements AutoCloseable {
     private ConnectionString connectionString = new ConnectionString("mongo://localhost:27017");
     private MongoCredential credential = MongoCredential.createCredential("admin","admin","adminpassword".toCharArray());
-    private CodeRegistry pojoCodecRegistry =
-            CodecRegistries.fromProviders(PojoCodecProvier.builder()
+    private CodecRegistry pojoCodecRegistry =
+            CodecRegistries.fromProviders(PojoCodecProvider.builder()
                     .automatic(true)
                     .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
                     .build());
     private MongoClient mongoClient;
-    private MongoDatabase rentACarDB; //rentACarDB thats from lecture xd
+    private MongoDatabase rentACarDB; //rentACarDB that's from lecture xd
 }
