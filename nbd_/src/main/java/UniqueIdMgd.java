24cc0b3cdbe6953a705f1104d5771a58dc95d62e
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-11-09 18:19:42 +0100

    class UniqueIdMgd created with usage of 'metoda na czuja'; not sure if the uuid should be auto generated when its not given to constructor

---
+++ nbd_/src/main/java/UniqueIdMgd.java
@@ -0,0 +1,19 @@
+import java.util.UUID;
+
+public class UniqueIdMgd {
+    private UUID uuid;
+
+    public UniqueIdMgd(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public UniqueIdMgd() {
+
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+
+}
