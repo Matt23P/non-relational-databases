09bbe4237553244a6a3faab6a10d6c84cfe4f69a
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-11-09 12:52:21 +0100

    method initializing connection with database implemented in AbstractMongoRepository class

--- nbd_/src/main/java/AbstractMongoRepository.java
+++ nbd_/src/main/java/AbstractMongoRepository.java
@@ -1,21 +1,42 @@
 import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoCredential;
 import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
 import com.sun.tools.javac.util.List;
+import org.bson.UuidRepresentation;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.pojo.Conventions;
 import org.bson.codecs.pojo.PojoCodecProvider;
 import org.bson.codecs.configuration.CodecRegistry;
 
+import java.io.IOException;
+
+
 public abstract class AbstractMongoRepository implements AutoCloseable {
     private ConnectionString connectionString = new ConnectionString("mongo://localhost:27017");
-    private MongoCredential credential = MongoCredential.createCredential("admin","admin","adminpassword".toCharArray());
+    private MongoCredential credential = MongoCredential.createCredential("admin", "admin", "adminpassword".toCharArray());
     private CodecRegistry pojoCodecRegistry =
             CodecRegistries.fromProviders(PojoCodecProvider.builder()
                     .automatic(true)
                     .conventions(List.of(Conventions.ANNOTATION_CONVENTION))
                     .build());
     private MongoClient mongoClient;
-    private MongoDatabase rentACarDB; //rentACarDB that's from lecture xd
+    private MongoDatabase sitterDB;
+
+    private void initDbConnection() {
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .credential(credential)
+                .applyConnectionString(connectionString)
+                .uuidRepresentation(UuidRepresentation.STANDARD)
+                .codecRegistry(CodecRegistries.fromRegistries(
+                        CodecRegistries.fromProviders(new UniqueIdCodecProvider()),
+                        MongoClientSettings.getDefaultCodecRegistry(),
+                        pojoCodecRegistry
+                )).build();
+
+        mongoClient = MongoClients.create(settings);
+        sitterDB = mongoClient.getDatabase("sitter");
+    }
 }
