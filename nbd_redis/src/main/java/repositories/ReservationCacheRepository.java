f0886e2ad09d006a7b5f751e99df402792551e05
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-11-29 21:32:46 +0100

    Reservation cache repository 50% done; CRUD operations need to be implemented

---
+++ nbd_redis/src/main/java/repositories/ReservationCacheRepository.java
@@ -0,0 +1,74 @@
+package repositories;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import entity.UniqueId;
+import model.Reservation;
+import redis.clients.jedis.*;
+
+public class ReservationCacheRepository extends ReservationRepository {
+    private JedisPooled pool;
+    private Jedis jedis;
+    private Gson gson;
+    private long lastCheck;
+    private boolean connected;
+
+    public ReservationCacheRepository() {
+        try {
+            gson = new GsonBuilder().create();
+            JedisClientConfig clientConfig = DefaultJedisClientConfig.builder().socketTimeoutMillis(100).build();
+            jedis = new Jedis(new HostAndPort("localhost",6379),clientConfig);
+            pool.set("ping","ping");
+            connected = true;
+        } catch (Exception exception) {
+            connected = false;
+        }
+    }
+
+    private boolean healthCheck() {
+        if (System.currentTimeMillis() - lastCheck < 60000) {return false;}
+        try { pool.set("ping","ping"); }
+        catch (Exception exception) { return false; }
+        if(!connected) {
+            jedis.flushDB();
+            connected = true;
+        }
+        return true;
+    }
+
+    private void jedisConnectionExceptionHandler(Exception exception){
+        exception.printStackTrace();
+        connected = false;
+        lastCheck = System.currentTimeMillis();
+    }
+
+//    private void addToCache(Reservation reservation) {
+//        pool.jsonSet("Reservation:" + reservation.getReservation???XDDDKurwachujpierdole)
+//    }
+
+    @Override
+    public Reservation add(Reservation reservation) {
+
+    }
+
+    @Override
+    public void remove(Reservation reservation) {
+
+    }
+
+    @Override
+    public Reservation getByUniqueId(UniqueId uniqueId) {
+
+    }
+
+    @Override
+    public boolean update(Reservation reservation) {
+
+    }
+
+    @Override
+    public Reservation getByReservation(Long number) {
+
+    }
+
+}
