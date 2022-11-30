c59561580a040758fc44f71b4b51056093244a2e
236657 Wojciech Stefaniak <236657@edu.p.lodz.pl>
2022-11-30 17:59:33 +0100

    managers

--- nbd_redis/src/main/java/managers/ReservationManager.java
+++ nbd_redis/src/main/java/managers/ReservationManager.java
@@ -1,4 +1,34 @@
 package managers;
 
+import entity.UniqueId;
+import model.Parent;
+import model.Reservation;
+import model.Sitter;
+import repositories.ReservationRepository;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class ReservationManager {
+    ReservationRepository reservationRepository;
+    public ReservationManager(ReservationRepository reservationRepository){ this.reservationRepository = reservationRepository; }
+
+    public boolean add(LocalDate date, LocalTime startTime, LocalTime endTime, Parent parent, Sitter sitter){
+        if(sitter.isAvailable()){
+            sitter.setAvailable(false);
+            Reservation reservation = new Reservation(date, startTime, endTime, parent, sitter);
+            return reservationRepository.add(reservation) != null;
+        }
+        return false;
+    }
+
+    public Reservation get(Reservation reservation){ return reservationRepository.get(reservation); }
+
+    public Reservation get(UniqueId entityId){ return reservationRepository.getByEntityId(entityId); }
+
+    public void update(Reservation reservation){ reservationRepository.update(reservation); }
+
+    public void remove(Reservation reservation){ reservationRepository.remove(reservation); }
+
+    public long getSize(){ return reservationRepository.getCollectionSize(); }
 }
