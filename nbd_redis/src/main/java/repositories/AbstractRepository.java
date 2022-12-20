c4a92c8c222d525d6a7b3d0d7e32b201689f9ec1
Mateusz Przybyl <236630@edu.p.lodz.pl>
2022-12-20 01:34:18 +0100

    body of AbstractRepository changed for Cassandra; (tutaj wojtas nwm czy nie powiazac tych tabel w sensie parent i sitter w reservation dac withPartitionKey i podac te pola jako UUID, czyli dawac tam ID tych dwoch a nie cale obiekty, elo)

--- nbd_redis/src/main/java/repositories/AbstractRepository.java
+++ nbd_redis/src/main/java/repositories/AbstractRepository.java
@@ -2,7 +2,16 @@ package repositories;
 
 
 import java.net.InetSocketAddress;
+
+import com.datastax.oss.driver.api.core.CqlIdentifier;
 import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.SimpleStatement;
+import com.datastax.oss.driver.api.core.type.DataTypes;
+import com.datastax.oss.driver.api.querybuilder.SchemaBuilder;
+import com.datastax.oss.driver.api.querybuilder.schema.CreateKeyspace;
+
+import static com.datastax.oss.driver.api.querybuilder.SchemaBuilder.createKeyspace;
+import static entity.CassandraNamespaces.*;
 
 public abstract class AbstractRepository {
     private static CqlSession session;
@@ -12,7 +21,48 @@ public abstract class AbstractRepository {
                 .addContactPoint(new InetSocketAddress("cassandra2", 9043))
                 .withLocalDatacenter("dc1")
                 .withAuthCredentials("cassandra", "cassandrapassword")
-//                .withKeyspace(CqlIdentifier.fromCql("???"))
+                .withKeyspace(CqlIdentifier.fromCql("sitter"))
                 .build();
+
+        CreateKeyspace keyspace = createKeyspace(SITTER_NAMESPACE)
+                .ifNotExists()
+                .withSimpleStrategy(2)
+                .withDurableWrites(true);
+        SimpleStatement createKeyspace = keyspace.build();
+        session.execute(createKeyspace);
+
+        SimpleStatement createParents = SchemaBuilder.createTable(PARENT_ID)
+                .ifNotExists()
+                .withPartitionKey(CqlIdentifier.fromCql("ID"), DataTypes.UUID)
+                .withColumn(CqlIdentifier.fromCql("name"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("address"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("phoneNumber"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("childAge"), DataTypes.INT)
+                .build();
+        session.execute(createParents);
+
+        SimpleStatement createSitters = SchemaBuilder.createTable(SITTER_ID)
+                .ifNotExists()
+                .withPartitionKey(CqlIdentifier.fromCql("ID"), DataTypes.UUID)
+                .withColumn(CqlIdentifier.fromCql("firstName"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("lastName"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("sitterType"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("basePrice"), DataTypes.DOUBLE)
+                .withColumn(CqlIdentifier.fromCql("skill"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("minAge"), DataTypes.INT)
+                .withColumn(CqlIdentifier.fromCql("available"), DataTypes.BOOLEAN)
+                .build();
+        session.execute(createSitters);
+
+        SimpleStatement createReservations = SchemaBuilder.createTable(RESERVATION_ID)
+                .ifNotExists()
+                .withPartitionKey(CqlIdentifier.fromCql("ID"), DataTypes.UUID)
+                .withColumn(CqlIdentifier.fromCql("date"), DataTypes.DATE)
+                .withColumn(CqlIdentifier.fromCql("startTime"), DataTypes.TIME)
+                .withColumn(CqlIdentifier.fromCql("endTime"), DataTypes.TIME)
+                .withColumn(CqlIdentifier.fromCql("parent"), DataTypes.TEXT)
+                .withColumn(CqlIdentifier.fromCql("sitter"), DataTypes.TEXT)
+                .build();
+        session.execute(createReservations);
     }
 }
